92d40c4068dc74ec19cdc93d8a8b2f0666438e8b
Jorge Navarro <jnavarro@okode.com>
2020-02-19 15:31:45 +0100

    WIP

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Smartlock } from '../../services/smartlock.service';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @Component({
   selector: 'page-home',
@@ -7,10 +9,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  name: string;
+  password: string;
+  result: string;
+
   constructor(
     public navCtrl: NavController,
-    ) {
+    private smartlock: Smartlock,
+    private splash: SplashScreen
+    ) {}
 
+  onSave() {
+    this.smartlock.save({
+      id: this.name,
+      name: this.name,
+      password: this.password
+    })
+  }
+
+  onRequest() {
+    this.splash.hide();
+    this.smartlock.request()
+    .then(({ id, name, password }) => {
+      this.result = 'id: ' + id + 'name: ' + name + 'password: ' + password;
+    })
+    .catch(err => console.log('Smartlock request error', err));
   }
 
 }
